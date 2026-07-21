9bcfd7e7132c3de1e998ed417a59e3d1751478d2
MikeJohnshoy <mike.johnshoy@gmail.com>
2026-07-21 09:00:42 -0400

    Update version string in sdr_ui.h
    WF display correct with center_bin moved from 512

--- src/sdr_ui.h
+++ src/sdr_ui.h
@@ -57,4 +57,4 @@ void update_log_ed();
 void write_call_log();
 time_t time_sbitx();
 
-#define VER_STR "zbitx v5.03 cw_pitch does not TX change freq"
+#define VER_STR "zbitx v5.03 WF display correct with center_bin <> 512"
